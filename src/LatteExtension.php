c1ba1f07c14e991fa2c164a47f0c9ca52788b146
Tomas Kudelka <tomas@appsdevteam.com>
2022-06-04 18:42:59 +0200

    init commit

--- src/LatteExtension.php
+++ src/LatteExtension.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace ADT\LatteExtension;
 
+use Exception;
 use Latte;
 
 final class LatteExtension extends Latte\Extension
 {
 	private ?string $timestamp = null;
 
+	/**
+	 * @throws Exception
+	 */
 	public function __construct(private readonly ?string $timestampFile = null)
 	{
 		if ($timestampFile && !is_file($timestampFile)) {
-			throw new \Exception("File '$timestampFile' not found or is not a file!");
+			throw new Exception("File '$timestampFile' not found or is not a file!");
 		}
 	}
 
@@ -25,14 +29,14 @@ final class LatteExtension extends Latte\Extension
 		];
 	}
 
-	public function createV(Latte\Compiler\Tag $tag): Latte\Compiler\Node
+	public function createV(): Latte\Compiler\Node
 	{
 		return new Latte\Compiler\Nodes\AuxiliaryNode(
 			fn (Latte\Compiler\PrintContext $context) => $context->format("echo '?v=". $this->getVersion() ."'")
 		);
 	}
 
-	public function createVn(Latte\Compiler\Tag $tag): Latte\Compiler\Node
+	public function createVn(): Latte\Compiler\Node
 	{
 		return new Latte\Compiler\Nodes\AuxiliaryNode(
 			fn (Latte\Compiler\PrintContext $context) => $context->format("echo '" . $this->getVersion() . "'")
@@ -48,7 +52,7 @@ final class LatteExtension extends Latte\Extension
 		$this->timestamp = '';
 
 		if ($this->timestampFile) {
-			$this->timestamp = (string) filemtime($this->$timestampFile);
+			$this->timestamp = (string) filemtime($this->timestampFile);
 		}
 
 		return $this->timestamp;
